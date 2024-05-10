f2f1b3b5af99e0ae8fc2d751ba2d0155148283ec
Zchouaibi <126184912+Zchouaibi@users.noreply.github.com>
2024-05-10 14:04:10 +0100

    up incidenceController and incidencia model

--- app/Http/Controllers/IncidenceController.php
+++ app/Http/Controllers/IncidenceController.php
@@ -12,35 +12,114 @@ class IncidenceController extends Controller
      * Display a listing of the incidences
      */
 
+    public function index()
+    {
+        $this->authorize('viewAny',Incidencia::class);
 
+        $incident = Incidencia::where('user_id', Auth::id())->get();
+        return view('incidences.index', ['incidebes' => $incident]);
+    }
 
      /**
       * Show the form for creating a new incidence
       */
+    public function create()
+    {
+        $this->authorize('create', Incidencia::class);
 
+        return view('incidences.create');
+    }
 
       /**
        * Store a newly created incidence in db
        */
+    public function store (Request $request)
+    {
+        $this->authorize('create', Incidencia::class);
+        $validatedData = $request->validate([
+            'description' => 'required|max:255',
+            'status' => 'required',
+            'priority' => 'required',
+            'location' => 'required',
+            'image' => 'sometimes|image|max:5000',
+        ]);
+        $incident = new Incidencia($validatedData);
+        $incident->user_id = Auth::id();
 
+        if ($request->hasFile('image')) {
+            $incident->image = $request->file('image')->store('public/incidences');
+        }
+
+        $incident->save();
+
+        return redirect()->Route('incidences.index')->with('success', 'Incidence created successfully');
+
+    }
        
        /**
         * Display the specified incidence
         */
+
+        public function show(Incidence $incidence)
+        {
+            $this->authorize('view', $incidence);
+    
+            return view('incidences.show', ['incidence' => $incidence]);
+        }
 
 
        /**
         * Show the form for editing specified incidence 
         */
 
+
+        public function edit(Incidence $incidence)
+        {
+            $this->authorize('update', $incidence);
+    
+            return view('incidences.edit', ['incidence' => $incidence]);
+        }
+
+
         /**
          * Update the specified incidence in storage
          */
+
+
+         public function update(Request $request, Incidence $incidence)
+    {
+        $this->authorize('update', $incidence);
+
+        $validatedData = $request->validate([
+            'description' => 'required|max:255',
+            'status' => 'required',
+            'priority' => 'required',
+            'location' => 'required',
+            'image' => 'sometimes|image|max:5000',
+        ]);
+
+        if ($request->hasFile('image')) {
+            $incidence->image = $request->file('image')->store('public/incidences');
+        }
+
+        $incidence->update($validatedData);
+
+        return redirect()->route('incidences.index')->with('success', 'Incidence updated successfully.');
+    }
 
 
          /**
           * Remove the specified incidence in db
           */
 
+
+          public function destroy(Incidence $incidence)
+    {
+        $this->authorize('delete', $incidence);
+
+        $incidence->delete();
+
+        return redirect()->route('incidences.index')->with('success', 'Incidence deleted successfully.');
+    }
 
 }
--- app/Models/Incidencia.php
+++ app/Models/Incidencia.php
@@ -8,4 +8,40 @@ use Illuminate\Database\Eloquent\Model;
 class Incidencia extends Model
 {
     use HasFactory;
+
+    protected $table = 'incidencias';
+
+    protected $fillable = [
+        'Descripcio',
+        'Data',
+        'Estat',
+        'Prioritat',
+        'Ruta_img',
+        'Rol_assignat',
+        'Ruta_proforma',
+        'Zona',
+        'Usuari_denunciant',
+    ];
+
+    protected $casts = [
+        'Data' => 'date',
+    ]
+
+    /**
+     * Return the zone that the incidence belongs to
+     */
+
+    public function zona()
+    {
+        return $this->belongsTo(Zona::class, 'Zona');
+    }
+
+    /**
+     * Get the user that reported the incidence
+     */
+
+    public function UsuariDenunciant()
+    {
+        return $this->belongsTo(Usuari::class, 'Usuari_denunciant');
+    }
 }
